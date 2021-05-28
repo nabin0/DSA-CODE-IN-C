5e1a819befb778234f0eb8fb3d152aa5c658ceb1
Nabin <nabinbhatt62@gmail.com>
2021-05-28 21:20:04 +0530

    added file

--- DSA Exercise/Array/4_sortWithoutAlgo.cpp
+++ DSA Exercise/Array/4_sortWithoutAlgo.cpp
@@ -1,35 +1,34 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-void prinData(vector<int> &v)
-{
-    cout<<"Data in vector is as:"<<endl;
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
-    }
+void sortArr(int arr[],int size, int min, int max ){
+    int n = max - min +1;
+
+    vector<int> v(n, 0);
+    for (int i = 0; i < size; i++)
+    v[arr[i] - min]++;
+
+    for (int i=0; i<n; i++)
+	for (int j=0; j < v[i]; j++)
+	cout << (i + min) << " ";
+
+    cout<<endl;
+
 }
 
-int sortArr(int arr[], int size, int min, int max)
-{
-int n = max-min+1;
-    vector<int> v(n,0);
-    for (int i = 0; i < n; i++)
-    {
-        v[arr[i] - min]++;
-    }
-    prinData(v);
+int main(){
 
-    return 0;
+int arr[] = {1,2,0};
+int min = 0, max = 2;
+int size = sizeof(arr)/sizeof(int);
+cout<<"Calling function"<<endl;
+sortArr(arr,size,min,max);
+cout<<"Code Executed"<<endl;
+return 0;
 }
 
-int main()
-{
-    cout<<"DONE"<<endl;
-    int arr[] = {0, 34, 100, 54, 76};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int min = 0, max = 100;
-    sortArr(arr, size,min,max);
-    cout<<"DONE"<<endl;
-    return 0;
-}
+
+
+
+
